918d0e0f1b53ed2d62eecfe5b2ee52dd412287ff
SAMtoBAM <31311492+SAMtoBAM@users.noreply.github.com>
2025-05-30 15:07:27 -0500

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+
+# copy git directory structure for stargraph to conda package location
+cd ${SRC_DIR}
+mkdir -p $PREFIX/main $PREFIX/aux $PREFIX/db $PREFIX/lib
+cp -r bin/* $PREFIX/bin/
+
+
+# compile CNEFinder
+git clone --branch main https://github.com/egluckthaler/starfish.git starfish
+cd starfish/CNEFinder/
+./pre-install.sh
+make -f Makefile
+mv cnef ../bin/
+cd ..
+
+# copy git directory structure for starfish to conda package location
+cp -r aux/* $PREFIX/aux/
+cp -r main/* $PREFIX/main/
+cp -r bin/* $PREFIX/bin/
+cp -r db/* $PREFIX/db/
+cp -r lib/* $PREFIX/lib/
