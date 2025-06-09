364012ac261b11310a2f4925052e80455fc11acc
SAMtoBAM <31311492+SAMtoBAM@users.noreply.github.com>
2025-06-09 20:59:55 +0200

    Add files via upload

--- bin/gggenomes_skeleton.R
+++ bin/gggenomes_skeleton.R
@@ -15,8 +15,10 @@ bed=read.csv("PATHTOOUTPUT/CLUSTER.regions_plus_flank.plotting.bed", sep='\t', h
 ##make another header, copy of contig, but called seq_id
 bed$seq_id = bed$contig
 ##also length using the end position
-bed$length = bed$end
-#bed$length = bed$end - bed$start
+#bed$length = bed$end
+bed$length = bed$end - bed$start
+##add a column with label to be used in the plot (showing the actual region being aligned)
+bed$label= paste(bed$contig,":",bed$start,"-",bed$end, sep = "")
 
 ##second feature
 ##a bed file of just the Starship-like region coordinates i.e. the above bed file without the flanking regions
@@ -39,7 +41,7 @@ links=read_links("PATHTOOUTPUT/CLUSTER.contigs.nucmer.paf")
 
 
 ##the actual plot
-## only selecting alignments greater than 5kb and with greater then _(% identity)
+## only selecting alignments greater than 1kb and with greater then 80% identity)
 gggenomes(genes=genes, seqs=bed, feat=SLRbed, links=subset(links, map_length > 1000 & map_match/map_length > 0.8 & seq_id != seq_id2), adjacent_only = T) %>%
   gggenomes::sync() %>%
   gggenomes::pick() %>%
@@ -50,7 +52,7 @@ gggenomes(genes=genes, seqs=bed, feat=SLRbed, links=subset(links, map_length > 1
   geom_seq(linewidth = 0.5)+
   geom_feat(color="red", alpha=.6, linewidth=3)+
   geom_gene(aes(fill=label), stroke=0.1, colour="black", shape = 3)+
-  geom_seq_label()+
+  geom_seq_label(aes(label=label))+
   geom_seq_label(aes(label=SLR), nudge_y = -.25)+
   geom_gene_tag(aes(label=label), size = 2, nudge_y=0.1, check_overlap = FALSE)+
   scale_fill_manual(values = c("red","blue","lightblue"), breaks=c("tyrR","myb", "duf3723"))+
@@ -67,7 +69,7 @@ plot=gggenomes(genes=genes, seqs=bed, feat=SLRbed, links=subset(links, map_lengt
   geom_seq(linewidth = 0.5)+
   geom_feat(color="red", alpha=.6, linewidth=3)+
   geom_gene(aes(fill=label), stroke=0.1, colour="black", shape = 3)+
-  geom_seq_label()+
+  geom_seq_label(aes(label=label))+
   geom_seq_label(aes(label=SLR), nudge_y = -.25)+
   geom_gene_tag(aes(label=label), size = 2, nudge_y=0.1, check_overlap = FALSE)+
   scale_fill_manual(values = c("red","blue","lightblue"), breaks=c("tyrR","myb", "duf3723"))+
