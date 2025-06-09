1c67e58a888b7884d01969b2b32fd48cb36d0420
SAMtoBAM <31311492+SAMtoBAM@users.noreply.github.com>
2025-06-09 17:22:23 +0200

    Add files via upload

---
+++ bin/gggenomes_skeleton.R
@@ -0,0 +1,82 @@
+###this is a modifiable Rscript for generating Starship alignments
+##the basic skeleton will remain the same and the same features can be swapped in for each variable
+
+##only need gggenomes
+library(IRanges)
+library(gggenomes)
+library(ggnewscale)
+
+##need four features
+
+##First feature
+##first a bed file with just the length and size of the full alignment regions
+bed=read.csv("PATHTOOUTPUT/CLUSTER.regions_plus_flank.plotting.bed", sep='\t', header=T)
+##just modify some headers for downstream handling
+##make another header, copy of contig, but called seq_id
+bed$seq_id = bed$contig
+##also length using the end position
+bed$length = bed$end
+#bed$length = bed$end - bed$start
+
+##second feature
+##a bed file of just the Starship-like region coordinates i.e. the above bed file without the flanking regions
+SLRbed=read.csv("PATHTOOUTPUT/CLUSTER.SLRs.bed", sep='\t', header=T)
+SLRbed$seq_id = SLRbed$contig
+SLRbed$length = SLRbed$end-SLRbed$start
+
+##third feature
+##a bed file with the genes annotated within the Starship-like regions (coordinates modified due to the flanking regions added)
+genes=read.csv("PATHTOOUTPUT/genes.bed", sep='\t', header=T)
+genes$seq_id = genes$contig
+genes$length= genes$end-genes$start
+genes$strand=genes$sense
+
+##fourth feature
+##the nucmer all-v-all alignment converted to paf format
+links=read_links("PATHTOOUTPUT/CLUSTER.contigs.nucmer.paf")
+
+
+
+
+##the actual plot
+## only selecting alignments greater than 5kb and with greater then _(% identity)
+gggenomes(genes=genes, seqs=bed, feat=SLRbed, links=subset(links, map_length > 1000 & map_match/map_length > 0.8 & seq_id != seq_id2), adjacent_only = T) %>%
+  gggenomes::sync() %>%
+  gggenomes::pick() %>%
+  gggenomes::flip() +
+  geom_link(aes(fill=map_match/map_length) ,colour="black", alpha=0.5, offset = 0.05, size=0.1 )+
+  scale_fill_gradientn(colours=c("grey100","grey75", "grey50"), name ="Identity", labels=c(.80,0.90,1), breaks=c(0.80,0.90,1), limits = c(0.8, 1))+
+  new_scale_fill()+
+  geom_seq(linewidth = 0.5)+
+  geom_feat(color="red", alpha=.6, linewidth=3)+
+  geom_gene(aes(fill=label), stroke=0.1, colour="black", shape = 3)+
+  geom_seq_label()+
+  geom_seq_label(aes(label=SLR), nudge_y = -.25)+
+  geom_gene_tag(aes(label=label), size = 2, nudge_y=0.1, check_overlap = FALSE)+
+  scale_fill_manual(values = c("red","blue","lightblue"), breaks=c("tyrR","myb", "duf3723"))+
+  theme(legend.position="top", legend.box = "horizontal")
+
+##save plot as variable so can save it
+plot=gggenomes(genes=genes, seqs=bed, feat=SLRbed, links=subset(links, map_length > 1000 & map_match/map_length > 0.8 & seq_id != seq_id2), adjacent_only = T) %>%
+  gggenomes::sync() %>%
+  gggenomes::pick() %>%
+  gggenomes::flip() +
+  geom_link(aes(fill=map_match/map_length) ,colour="black", alpha=0.5, offset = 0.05, size=0.1 )+
+  scale_fill_gradientn(colours=c("grey100","grey75", "grey50"), name ="Identity", labels=c(.80,0.90,1), breaks=c(0.80,0.90,1), limits = c(0.8, 1))+
+  new_scale_fill()+
+  geom_seq(linewidth = 0.5)+
+  geom_feat(color="red", alpha=.6, linewidth=3)+
+  geom_gene(aes(fill=label), stroke=0.1, colour="black", shape = 3)+
+  geom_seq_label()+
+  geom_seq_label(aes(label=SLR), nudge_y = -.25)+
+  geom_gene_tag(aes(label=label), size = 2, nudge_y=0.1, check_overlap = FALSE)+
+  scale_fill_manual(values = c("red","blue","lightblue"), breaks=c("tyrR","myb", "duf3723"))+
+  theme(legend.position="top", legend.box = "horizontal")
+
+
+ggsave(filename="PATHTOOUTPUT/CLUSTER.pdf", 
+       plot = plot, 
+       device = cairo_pdf, 
+       width = 210, 
+       height = 297, 
+       units = "mm")
