f25754bd4dd04de22b354a895cbabbeb93e5683d
SAMtoBAM <31311492+SAMtoBAM@users.noreply.github.com>
2025-06-19 11:50:21 -0500

    Update gggenomes_skeleton.stargraph.R

--- bin/gggenomes_skeleton.stargraph.R
+++ bin/gggenomes_skeleton.stargraph.R
@@ -65,6 +65,6 @@ widthFrac = max(bed$length+100000) / 20000
 #heightFrac = nrow(regionSeqs) / 2 # for default cases
 heightFrac = nrow(bed) / 0.5 # for when OG ids are included as gene_name
 
-ggsave("PATHTOOUTPUT/CLUSTER.png", plot = plot, units = "in", height = heightFrac, width = widthFrac, limitsize = FALSE)
-ggsave("PATHTOOUTPUT/CLUSTER.svg", plot = plot, units = "in", height = heightFrac, width = widthFrac, limitsize = FALSE)
+suppressMessages(suppressWarnings(ggsave("PATHTOOUTPUT/CLUSTER.png", plot = plot, units = "in", height = heightFrac, width = widthFrac, limitsize = FALSE)))
+suppressMessages(suppressWarnings(ggsave("PATHTOOUTPUT/CLUSTER.svg", plot = plot, units = "in", height = heightFrac, width = widthFrac, limitsize = FALSE)))
 
