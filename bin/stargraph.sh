1fb7ae4a9bf545936ae4e9623777a03fc7097c56
SAMtoBAM <31311492+SAMtoBAM@users.noreply.github.com>
2025-09-04 17:54:07 -0500

    Update stargraph.sh

--- bin/stargraph.sh
+++ bin/stargraph.sh
@@ -208,6 +208,11 @@ case "$key" in
 	"
 	exit
 	;;
+	*)  # catch invalid args
+    echo "ERROR: Unknown option: '$1'"
+    echo "Run 'stargraph.sh -h' to see valid options"
+    exit 1
+    ;;
 	esac
 done
 
